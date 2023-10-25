9b0bf0135b720445d4223407bd1d00b875b5ad9c
Yash Mhaskar <mhaskaryash901@gmail.com>
2023-10-25 14:54:31 -0400

    Add files via upload

---
+++ team19_navigate_to_goal/TwistControl.py
@@ -0,0 +1,77 @@
+# Joseph Sommer and Yash Mhaskar
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist
+import numpy
+from simple_pid import PID
+
+class TwistSubPub(Node):
+	def __init__(self):
+		super().__init__('twist_sub_pub')
+		self.subscription = self.create_subscription(Float32MultiArray, 'heading', self.mover_callback, 5)
+		self.subscription
+		self.cmd_vel = self.create_publisher(Twist,'cmd_vel',10)
+
+	def mover_callback(self, msg):
+		mover_data = msg.data
+		e_l = float(mover_data[0])
+		e_a = float(mover_data[1])
+		e_a = e_a*3.14159/180
+		e_a = numpy.arctan2(numpy.sin(e_a), numpy.cos(e_a))
+
+		setlin = 0.5
+		setang = 0.0
+		bufferlin = 0.1
+		bufferang = 0.2		
+
+		pid_l = PID(-0.45,0.000,0.05,setpoint=setlin)
+		v_l = pid_l(e_l)
+		pid_a = PID(-0.6,0.0,0.05,setpoint=setang)
+		v_a = pid_a(e_a)
+		
+		if v_l > 0.22:
+			v_l = 0.22
+		elif v_l < -0.22:
+			v_l = -0.22
+		if v_a > 1.42:
+			v_a = 1.42
+		elif v_a < -1.42:
+			v_a = -1.42
+		
+		twist = Twist()
+		if abs(e_a) > 0.2:
+			self.get_logger().info('Adjusting Angular twist')
+			#self.get_logger().info('e_l: "%s"'% e_l)
+			#self.get_logger().info('v_l: "%s"'% v_l)
+			twist.angular.z = v_a
+			twist.linear.x = 0.0
+			
+		if abs(e_a) < 0.2:
+			self.get_logger().info('Adjusting Linear twist')
+			#self.get_logger().info('e_l: "%s"'% e_l)
+			#self.get_logger().info('v_l: "%s"'% v_l)
+			twist.angular.z = 0.0
+			twist.linear.x = v_l
+
+		if abs(e_l)<0.02:
+			self.get_logger().info('Reached point')
+			#self.get_logger().info('e_a: "%s"'% e_a)
+			#self.get_logger().info('e_l: "%s"'% e_l)
+			twist.angular.z = 0.0
+			twist.linear.x = 0.0
+			
+		
+		self.cmd_vel.publish(twist)
+
+def main(args=None):
+
+	rclpy.init(args=args)
+	twist_sub_pub = TwistSubPub()
+	rclpy.spin(twist_sub_pub)
+	twist_sub_pub.destroy_node()
+	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
---
+++ team19_navigate_to_goal/goToGoal.py
@@ -0,0 +1,157 @@
+# Joseph Sommer and Yash Mhaskar
+# Description: Subscribes to /odom data and the getObjectRange data and determines a heading
+
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Point
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import LaserScan
+import time
+
+# etc
+import numpy as np
+import math
+
+#Global Variables
+global state
+state = 1
+
+class Goal_Pub_Sub(Node):
+	def __init__(self):
+		super().__init__('goal_pub_sub')
+		# State (for the update_Odometry code)
+		self.Init = True
+		self.Init_pos = Point()
+		self.Init_pos.x = 0.0
+		self.Init_pos.y = 0.0
+		self.Init_ang = 0.0
+		self.globalPos = Point()
+
+		self.odom_sub = self.create_subscription(
+			Odometry,
+			'/odom',
+			self.odom_callback,
+			1)
+		self.odom_sub  # prevent unused variable warning
+		self.heading = self.create_publisher(Float32MultiArray,'heading',10)
+
+
+	def odom_callback(self, data):
+		self.update_Odometry(data)
+
+	def update_Odometry(self,Odom):
+		position = Odom.pose.pose.position
+		global state
+		#Orientation uses the quaternion aprametrization.
+		#To get the angular position along the z-axis, the following equation is required.
+		q = Odom.pose.pose.orientation
+		orientation = np.arctan2(2*(q.w*q.z+q.x*q.y),1-2*(q.y*q.y+q.z*q.z))
+
+		if self.Init:
+			#The initial data is stored to by subtracted to all the other values as we want to start at position (0,0) and orientation 0
+			self.Init = False
+			self.Init_ang = orientation
+			self.globalAng = self.Init_ang
+			Mrot = np.matrix([[np.cos(self.Init_ang), np.sin(self.Init_ang)],[-np.sin(self.Init_ang), np.cos(self.Init_ang)]])        
+			self.Init_pos.x = Mrot.item((0,0))*position.x + Mrot.item((0,1))*position.y
+			self.Init_pos.y = Mrot.item((1,0))*position.x + Mrot.item((1,1))*position.y
+			self.Init_pos.z = position.z
+		Mrot = np.matrix([[np.cos(self.Init_ang), np.sin(self.Init_ang)],[-np.sin(self.Init_ang), np.cos(self.Init_ang)]])        
+
+		#We subtract the initial values
+		self.globalPos.x = Mrot.item((0,0))*position.x + Mrot.item((0,1))*position.y - self.Init_pos.x
+		self.globalPos.y = Mrot.item((1,0))*position.x + Mrot.item((1,1))*position.y - self.Init_pos.y
+		self.globalAng = orientation - self.Init_ang
+	
+		self.get_logger().info('Transformed global pose is x:{}, y:{}, a:{}'.format(self.globalPos.x,self.globalPos.y,self.globalAng))
+
+		xodom = self.globalPos.x
+		yodom = self.globalPos.y
+		curPos = [xodom, yodom]
+		theta_odom = self.globalAng
+		lin_err_range = 0.05
+	
+		if state==1: # State 1: (0,0) to (1.5, 0)
+			goal = [1.5, 0]
+			e_l = distance_to_goal = math.sqrt((goal[0] - curPos[0]) ** 2 + (goal[1] - curPos[1]) ** 2)
+			e_a = math.atan2(goal[1] - curPos[1], goal[0] - curPos[0]) - theta_odom
+			if e_l > lin_err_range:
+				msg = Float32MultiArray()
+				msg.data = [e_l,e_a]
+				# Publish the x-axis position
+				self.heading.publish(msg)
+			elif e_l < lin_err_range:
+				state = 2
+				e_l = 0
+				e_a = 0
+				# publish e_l and e_a self.cmd_vel.publish(twist)
+				msg = Float32MultiArray()
+				msg.data = [e_l, e_a]
+				self.heading.publish(msg)
+				time.sleep(10)
+		elif state == 2: # State 2: (1.5, 0) to (2.25, 0.7)
+			goal = [2.25, 0.7]
+			e_l = distance_to_goal = math.sqrt((goal[0] - curPos[0]) ** 2 + (goal[1] - curPos[1]) ** 2)
+			e_a = math.atan2(goal[1] - curPos[1], goal[0] - curPos[0]) - theta_odom
+			if e_l > lin_err_range:
+				msg = Float32MultiArray()
+				msg.data = [e_l,e_a]
+				# Publish the x-axis position
+				self.heading.publish(msg)
+			elif e_l < lin_err_range:
+				state = 3
+				e_l = 0
+				e_a = 0
+				# publish e_l and e_a self.cmd_vel.publish(twist)
+				msg = Float32MultiArray()
+				msg.data = [e_l, e_a]
+				self.heading.publish(msg)
+				time.sleep(10)
+		elif state == 3: # State 3: (2.25, 0.7) to (1.5, 1.4)
+			goal = [1.5, 1.4]
+			e_l = distance_to_goal = math.sqrt((goal[0] - curPos[0]) ** 2 + (goal[1] - curPos[1]) ** 2)
+			e_a = math.atan2(goal[1] - curPos[1], goal[0] - curPos[0]) - theta_odom
+			if e_l > lin_err_range:
+				msg = Float32MultiArray()
+				msg.data = [e_l,e_a]
+				# Publish the x-axis position
+				self.heading.publish(msg)
+			elif e_l < lin_err_range:
+				state = 4
+				e_l = 0
+				e_a = 0
+				# publish e_l and e_a self.cmd_vel.publish(twist)
+				msg = Float32MultiArray()
+				msg.data = [e_l, e_a]
+				self.heading.publish(msg)
+				time.sleep(10)
+		elif state == 4: # State 4: (1.5, 1.4) to (0, 1.4)
+			goal = [0, 1.4]
+			e_l = distance_to_goal = math.sqrt((goal[0] - curPos[0]) ** 2 + (goal[1] - curPos[1]) ** 2)
+			e_a = math.atan2(goal[1] - curPos[1], goal[0] - curPos[0]) - theta_odom
+			if e_l > lin_err_range:
+				msg = Float32MultiArray()
+				msg.data = [e_l,e_a]
+				# Publish the x-axis position
+				self.heading.publish(msg)
+			elif e_l < lin_err_range:
+				state = 4
+				e_l = 0
+				e_a = 0
+				# publish e_l and e_a self.cmd_vel.publish(twist)
+				msg = Float32MultiArray()
+				msg.data = [e_l, e_a]
+				self.heading.publish(msg)
+				time.sleep(10)
+	
+def main(args=None):
+	rclpy.init(args=args)
+	goal_pub_sub = Goal_Pub_Sub()
+	rclpy.spin(goal_pub_sub)
+	goal_pub_sub.destroy_node()
+	rclpy.shutdown()
+
+if __name__=='__main__':
+	main()
